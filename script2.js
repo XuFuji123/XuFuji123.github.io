da6e2df66694f09d8ecfbafae4ccfe1511d1492b
XuFuji123 <100395565+XuFuji123@users.noreply.github.com>
2022-04-17 23:29:39 +0800

    Add index.html

---
+++ script2.js
@@ -0,0 +1,19 @@
+var response = prompt("What's your name?");
+alert("你好, " + response + " 我们为你准备了一个小谜题");
+
+function hahaha(){
+      alert("与字母的顺序有关");
+}
+
+function answer(){
+      alert("2739");
+}
+function daan(){
+  var a = prompt("正确答案是?");
+  if(a==2739){
+     (location.href='file:///D:/Github/20220225/index2.html');
+  }
+  else if(a!==2739){
+    alert("false");
+  }
+}
