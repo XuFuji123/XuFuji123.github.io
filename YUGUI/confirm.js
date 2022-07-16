349a57f66c6c0c3463e3fcbb2831a5f37e76bd20
XFJ <1353565647@qq.com>
2022-07-16 22:37:16 +0800

    html

--- YUGUI/confirm.js
+++ YUGUI/confirm.js
@@ -12,27 +12,27 @@ function MCK(){
 	return
 }
 	if(deathTime.value<6){
-		document.getElementById("tst1").innerHTML="<h1>天将初晓</br>晨间的风不经意拂过</br>穿着"+deathCloth+"</br>所有的约束在身边弥散</br>在"+deathLocation+"</br>你被世界创造</br>又被世界召回</h1>";
+		document.getElementById("tst1").innerHTML="<h1>天将初晓</br>晨间的风不经意拂过</br>穿着"+deathCloth+"</br>所有的约束在身边弥散</br>在"+deathLocation+"</br>你被世界创造</br>又被世界召来</h1>";
     document.getElementById("tst2").innerHTML="<h1>珍惜时光，请你一定要做的事情:</br>1."+toDo1+"</br>2."+toDo2+"</br>3."+toDo3+"</br>遗体处理方式："+howDeal+"</br>遗产分配方式："+theHeritage+"</h1>";
 }
   else if(deathTime.value<12){
-	  document.getElementById("tst1").innerHTML="<h1>大亮的天光</br>等着阳光洒落</br>盼着雨打芭蕉，穿着"+deathCloth+"</br>所有的约束在身边弥散</br>在"+deathLocation+"你被世界创造</br>又被世界召回</h1>";
+	  document.getElementById("tst1").innerHTML="<h1>大亮的天光</br>等着阳光洒落</br>盼着雨打芭蕉，穿着"+deathCloth+"</br>所有的约束在身边弥散</br>在"+deathLocation+"你被世界创造</br>又被世界召来</h1>";
     document.getElementById("tst2").innerHTML="<h1>珍惜时光，请你一定要做的事情:</br>1."+toDo1+"</br>2."+toDo2+"</br>3."+toDo3+"</br>遗体处理方式："+howDeal+"</br>遗产分配方式："+theHeritage+"</h1>";
 }
   else if(deathTime.value<14){
-	  document.getElementById("tst1").innerHTML="<h1>此刻你所处的位置</br>光垂直下落</br>穿着"+deathCloth+"</br>所有的约束在身边弥散</br>在"+deathLocation+"你被世界创造</br>又被世界召回</h1>"
+	  document.getElementById("tst1").innerHTML="<h1>此刻你所处的位置</br>光垂直下落</br>穿着"+deathCloth+"</br>所有的约束在身边弥散</br>在"+deathLocation+"你被世界创造</br>又被世界召来</h1>"
     document.getElementById("tst2").innerHTML="<h1>珍惜时光，请你一定要做的事情:</br>1."+toDo1+"</br>2."+toDo2+"</br>3."+toDo3+"</br>遗体处理方式："+howDeal+"</br>遗产分配方式："+theHeritage+"</h1>";
 }
   else if(deathTime.value<18){
-	  document.getElementById("tst1").innerHTML="<h1>地球自转</br>带着太阳从头顶移开</br>穿着"+deathCloth+"</br>所有的约束在身边弥散</br>在"+deathLocation+"你被世界创造</br>又被世界召回</h1>"
+	  document.getElementById("tst1").innerHTML="<h1>地球自转</br>带着太阳从头顶移开</br>穿着"+deathCloth+"</br>所有的约束在身边弥散</br>在"+deathLocation+"你被世界创造</br>又被世界召来</h1>"
     document.getElementById("tst2").innerHTML="<h1>珍惜时光，请你一定要做的事情:</br>1."+toDo1+"</br>2."+toDo2+"</br>3."+toDo3+"</br>遗体处理方式："+howDeal+"</br>遗产分配方式："+theHeritage+"</h1>";
 }
   else if(deathTime.value<20){
-	  document.getElementById("tst1").innerHTML="<h1>在日夜流转的缝隙里</br>身后有光</br>眼前有梦</br>穿着"+deathCloth+"</br>所有的约束在身边弥散</br>在"+deathLocation+"你被世界创造</br>又被世界召回</h1>"
+	  document.getElementById("tst1").innerHTML="<h1>在日夜流转的缝隙里</br>身后有光</br>眼前有梦</br>穿着"+deathCloth+"</br>所有的约束在身边弥散</br>在"+deathLocation+"你被世界创造</br>又被世界召来</h1>"
     document.getElementById("tst2").innerHTML="<h1>珍惜时光，请你一定要做的事情:</br>1."+toDo1+"</br>2."+toDo2+"</br>3."+toDo3+"</br>遗体处理方式："+howDeal+"</br>遗产分配方式："+theHeritage+"</h1>";
 }
   else if(deathTime.value<=24){
-	  document.getElementById("tst1").innerHTML="<h1>喧嚣归于寂静</br>夜幕低垂</br>穿着"+deathCloth+"</br>所有的约束在身边弥散</br>在"+deathLocation+"你被世界创造</br>又被世界召回</h1>"
+	  document.getElementById("tst1").innerHTML="<h1>喧嚣归于寂静</br>夜幕低垂</br>穿着"+deathCloth+"</br>所有的约束在身边弥散</br>在"+deathLocation+"你被世界创造</br>又被世界召来</h1>"
     document.getElementById("tst2").innerHTML="<h1>珍惜时光，请你一定要做的事情:</br>1."+toDo1+"</br>2."+toDo2+"</br>3."+toDo3+"</br>遗体处理方式："+howDeal+"</br>遗产分配方式："+theHeritage+"</h1>";
 }
 
